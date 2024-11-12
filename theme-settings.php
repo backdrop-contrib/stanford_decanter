2c0cce2112e2434747aed7084afdbf42475040e6
Ronan Dowling <ronan@dowlings.me>
2024-11-12 12:58:23 -0600

    Cleaned up color settings

--- theme-settings.php
+++ theme-settings.php
@@ -1,59 +1,6 @@
 <?php
 /**
  * @file
- * Theme settings for MyTheme.
+ * Theme settings for Stanford Decanter.
  */
 
-if (module_exists('color')) {
-  $form['header'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Header Settings'),
-    '#collapsible' => TRUE,
-  );
-
-
-  $fields = array(
-    'header',
-    'titleslogan',
-    'stanfordwordmark',
-
-    'menubg',
-    'menutext',
-    'menuhover'
-  );
-  foreach ($fields as $field) {
-    $form['header'][$field] = color_get_color_element($form['theme']['#value'], $field, $form);
-  }
-
-  $form['general'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('General Settings'),
-    '#collapsible' => TRUE,
-  );
-
-  $fields = array(
-    'bg',
-    'text',
-    'link',
-    'title',
-    'identitybg',
-    'identitytext',
-  );
-  foreach ($fields as $field) {
-    $form['general'][$field] = color_get_color_element($form['theme']['#value'], $field, $form);
-  }
-
-  $form['footer'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Footer Settings'),
-    '#collapsible' => TRUE,
-  );
-  $fields = array(
-    'footerborder',
-    'footer',
-    'footertext',
-  );
-  foreach ($fields as $field) {
-    $form['footer'][$field] = color_get_color_element($form['theme']['#value'], $field, $form);
-  }
-}
