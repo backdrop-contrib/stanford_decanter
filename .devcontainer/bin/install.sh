28f1135da2e168bb4c01107a93e9c4eff4b0a587
Ronan Dowling <ronan@dowlings.me>
2024-12-30 19:57:43 +0000

    Added install profile and install script

--- .devcontainer/bin/install.sh
+++ .devcontainer/bin/install.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
+# rm -rf /var/www/html/settings.php
+rm -rf /var/www/html/files/config*
+
 mariadb -h db --password=root -e 'DROP DATABASE IF EXISTS app; CREATE DATABASE app'
 
-rm -rf /var/www/html
-mkdir /var/www/html
-
-bee dl-core /var/www/html
-bee site-install --db-name=app --db-user=root --db-pass=root --db-host=db --db-prefix=''\
-                      --username=admin --password=admin --email=admin@example.com \
-                      --site-name="Decanter 7 Test" \
-                      --site-email="admin@example.com" \
-                      --langcode=en \
-                      --base-url=http://localhost:8000/ \
-                      --root=/var/www/html \
-                      --auto \
-
-chown -R www-data:www-data /var/www/html
+bee site-install \
+      --db-name=app --db-user=root --db-pass=root --db-host=db --db-prefix=''\
+      --auto\
+      --profile=decanter_profile \
+      --root=/var/www/html \
