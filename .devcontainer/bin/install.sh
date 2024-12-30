6ed5a682464329fe38a05a01d4a1fee48ebe8063
Ronan Dowling <ronan@dowlings.me>
2024-12-30 14:37:28 -0600

    Add devcontainer file with install profile (#22)

---
+++ .devcontainer/bin/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# rm -rf /var/www/html/settings.php
+rm -rf /var/www/html/files/config*
+
+mariadb -h db --user=root --password=root -e 'DROP DATABASE IF EXISTS app; CREATE DATABASE app'
+
+bee site-install \
+      --db-name=app --db-user=root --db-pass=root --db-host=db --db-prefix=''\
+      --auto\
+      --profile=decanter_profile \
+      --root=/var/www/html \
