d3467ca5de527be1f35f1bc15a4569748506bfd8
Ronan Dowling <ronan@dowlings.me>
2025-01-26 15:35:35 -0600

    Rebase changes onto the latest 2.x branch

--- .ops/vrt-scripts/onReady.js
+++ .ops/vrt-scripts/onReady.js
@@ -2,6 +2,6 @@ module.exports = async (page, scenario, viewport, isReference, browserContext) =
   console.log(`----------------------------------
 url: '${scenario.url}' 
 label:  '${scenario.label}'`);
-  
-  // await require('./clickAndHoverHelper')(page, scenario);
+
+  await require('./clickAndHoverHelper')(page, scenario);
 };
