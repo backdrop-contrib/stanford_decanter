4e0a6cca8b2b309c25923eea866a66c8916b4d93
Ronan Dowling <ronan@dowlings.me>
2025-01-26 21:28:08 +0000

    Menu improvements

--- .ops/vrt-scripts/clickMenu.js
+++ .ops/vrt-scripts/clickMenu.js
@@ -1,17 +1,21 @@
 module.exports = async (page, scenario) => {
+
+  // Desktop: Top Level menu link is visible
+  // const menu = await page.waitForSelector('a[href*="menus"]', {state: 'attached', timeout: 3000});
+  // if (menu && await menu.isVisible()) {
+  //   await menu.hover(); 
+  //   await menu.dispatchEvent('mousemove')
+  //   // #TODO: The playwrite hover does not trigger the menu js.
+  //   // await page.hover('a[href*="menus/long"]');
+  //   await page.waitForTimeout(10000);
+  // }
+
+  // Mobile: Menu toggle is visible
   const toggle = await page.waitForSelector('.menu-toggle-button', {state: 'attached', timeout: 3000});
   if (toggle && await toggle.isVisible()) {
     await page.click('.menu-toggle-button');
     await page.click('.l-header .block-system-main-menu .has-submenu');
     await page.click('a[href*="menus/long"]');
     await page.waitForTimeout(1000);
-  }
-
-  const hover = await page.waitForSelector('a[href*="menus"]', {state: 'attached', timeout: 3000});
-  if (hover && await hover.isVisible()) {
-    await page.hover('a[href*="menus"]');
-    // #TODO: The playwrite hover does not trigger the menu js.
-    // await page.hover('a[href*="menus/long"]');
-    // await page.waitForTimeout(10000);
   }
 };
