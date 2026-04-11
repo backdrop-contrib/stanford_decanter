42d7525781e7edf6048b1d0a781505e17be9ed87
Ronan Dowling <ronan@dowlings.me>
2026-04-11 00:19:44 -0500

    Refactor configuration files and enhance layout settings for improved functionality

--- .ops/settings.local.php
+++ .ops/settings.local.php
@@ -1,6 +1,6 @@
 <?php
 
-$config_directories['staging'] = '/workspace/profiles/decanter_profile/config/';
+// $config_directories['staging'] = '/workspace/profiles/decanter_profile/config/';
 $config['system.core']['config_sync_clear_staging'] = 0;
 
 if (!empty($_ENV['CODESPACE_NAME'])) {
